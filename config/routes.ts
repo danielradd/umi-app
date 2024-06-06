dba0a504e4f89c7bbef647df33ed8e229e6d4cfd
Daniel Radd <draddx@outlook.com>
2024-06-06 01:07:53 -0300

    setting internacionalization labels and creatin pae for crm system.

--- config/routes.ts
+++ config/routes.ts
@@ -1,21 +1,31 @@
 export default [
   {
-    path: '/',
-    redirect: '/app/login',
+    path: '/login',
+    component: '@/pages/Login',
+    layout: false,
   },
   {
-    path: '/app',
-    component: '@/layouts/Header',
-    routes: [
-      {
-        exact: false,
-        path: '/app/login',
-        component: '@/pages/Login',
-      },
-      {
-        path: '/app/home',
-        component: '@/pages/Home',
-      },
-    ],
+    path: '/',
+    name: 'home',
+    icon: 'home',
+    component: '@/pages/Home',
+  },
+  {
+    path: '/customers',
+    name: 'customers',
+    icon: 'user',
+    component: '@/pages/Customers',
+  },
+  {
+    path: '/opportunities',
+    name: 'opportunities',
+    icon: 'AccountBook',
+    component: '@/pages/Opportunities',
+  },
+  {
+    path: '/reports',
+    name: 'reports',
+    icon: 'BarChartOutlined',
+    component: '@/pages/Reports',
   },
 ];
